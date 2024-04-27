1fb44808760a1cdf281d7d74ee6a4361ccf86f6d
David N Rodrigues <124949322+DavidNR21@users.noreply.github.com>
2024-04-27 00:17:19 -0300

    atualização da tela filmes

--- src/components/BannerFilme/Filme.jsx
+++ src/components/BannerFilme/Filme.jsx
@@ -5,7 +5,8 @@ import './styles.css'
 
 function Filme (){
 
-    const filmes = [
+
+  const filmes = [
         {
           "dub_leg": "DUB",
           "duracao": "2h 18m",
@@ -64,34 +65,35 @@ function Filme (){
           }
     ];
     
-    return (
-        <div className="filmes-container">
-          {filmes.map(filme => (
-            <div key={filme.id} className="filme-card">
-              <img src={filme.imagem} alt={filme.nome_filme} className="filme-imagem" />
-              <div className="filme-info">
-                <div className='titulo-div'>
-                    <h3 className="filme-titulo">{filme.nome_filme} </h3>
-                </div>
 
-                <div className='detalhes-div'>
-                    <p className="filme-detalhes">Duração: {filme.duracao}</p>
-                    <p className="filme-detalhes">Horário: {filme.horarios}</p>
-                </div>
-
-                <div className='infos-div'>
-                    <p className="filme-detalhes">Preço: R${filme.preco_ingresso}</p>
-                    <p className="filme-detalhes">Sala: {filme.sala.nome_sala}</p>
-                </div>
-
-                <div className='ingresso-div'>
-                    <span>Ingresso</span>
-                </div>
-              </div>
+  return (
+    <div className="filmes-container">
+      {filmes.map(filme => (
+        <div key={filme.id} className="filme-card">
+          <img src={filme.imagem} alt={filme.nome_filme} className="filme-imagem" />
+          <div className="filme-info">
+            <div className='titulo-div'>
+              <h3 className="filme-titulo">{filme.nome_filme} </h3>
             </div>
-          ))}
+
+            <div className='detalhes-div'>
+              <p className="filme-detalhes">Duração: {filme.duracao}</p>
+              <p className="filme-detalhes">Horário: {filme.horarios}</p>
+            </div>
+
+            <div className='infos-div'>
+              <p className="filme-detalhes">Preço: R${filme.preco_ingresso}</p>
+              <p className="filme-detalhes">Sala: {filme.sala.nome_sala}</p>
+            </div>
+
+            <div className='ingresso-div'>
+              <span>Comprar Ingresso</span>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
     
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,17 +3,40 @@ import { useEffect, useState, useContext } from "react";
 import Header from "../../components/Header/Header";
 import Filme from "../../components/BannerFilme/Filme";
 import { CinemaContext } from '../../context/CinemaContext';
+import Select from "react-dropdown-select";
 import './styles.css'
 
 
 function Home (){
 
     const { count, emailUsuario } = useContext(CinemaContext)
+    const [value, setValue] = useState([])
 
+    const ar = [{id: 1, nome : "teste1"}, {id: 2, nome : "test2"} ,{id: 3, nome : "teste3"}]
 
     return(
         <div className="home">
             <Header />
+            <div className="dropdown">
+                <div className="drop-div">
+                    <Select placeholder="Selecione uma cidade"
+                        options={ar}
+                        labelField="nome"
+                        valueField="id"
+                        className="drop"
+                        onChange={e => setValue(e)}
+                    />
+                </div>
+                <div className="drop-div">
+                    <Select placeholder="Selecione um cinema"
+                        options={ar}
+                        labelField="nome"
+                        valueField="id"
+                        className="drop"
+                        onChange={e => setValue(e)}
+                    />
+                </div>
+            </div>
             <div className="filme-div">
                 <Filme />
             </div>
